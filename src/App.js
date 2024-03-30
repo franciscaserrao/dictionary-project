a57fae3aff5bb442076206e426a3177677e86432
franciscaserrao <francisca.s.serrao@gmail.com>
2024-03-30 19:53:07 +0000

    Created basic project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hi
+        <a href="/" className="btn btn-primary">
+          Search
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
